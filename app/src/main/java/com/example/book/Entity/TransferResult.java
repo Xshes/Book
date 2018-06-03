3bf2f2499a9d396332a429236853dd23274b8691
Thorn <youzijia555@outlook.com>
2018-06-03 20:40:15 +0800

    忘了写的啥

---
+++ app/src/main/java/com/example/book/Entity/TransferResult.java
@@ -0,0 +1,12 @@
+package com.example.book.Entity;
+
+import java.util.List;
+
+/**
+ * Created by youzi on 2018/5/31.
+ */
+
+public class TransferResult {
+    public String result;
+    public List<Transfer> list;
+}
